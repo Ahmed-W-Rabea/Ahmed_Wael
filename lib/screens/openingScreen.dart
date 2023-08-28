673af6a516c68db54eb7c8518678d8d73ebc54aa
Ahmed-W-Rabea <ahmedwael11425@gmail.com>
2023-08-28 15:08:12 +0300

    simple animation at opening screen

--- lib/screens/openingScreen.dart
+++ lib/screens/openingScreen.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/main.dart';
 import 'package:flutter_application_1/screens/loginScreen.dart';
+import 'package:flutter/animation.dart';
 
-class openingScreen extends StatelessWidget {
+class openingScreen extends StatefulWidget {
   const openingScreen({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<openingScreen> createState() => _openingScreenState();
+}
+
+class _openingScreenState extends State<openingScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _slideController =
+      AnimationController(vsync: this, duration: Duration(seconds: 2))
+        ..repeat(reverse: true);
+  late final Animation<Offset> _animated = Tween<Offset>(
+    begin: Offset.zero,
+    end: const Offset(1, 1.5),
+  ).animate(CurvedAnimation(parent: _slideController, curve: Curves.elasticIn));
+
+  void initstate() {
+    super.initState();
+
+    _slideController.forward();
+  }
+
+// This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,8 +44,10 @@ class openingScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Image(image: AssetImage("assets/images/R.png")),
-              const Text(
+              SlideTransition(
+                  position: _animated,
+                  child: const Image(image: AssetImage("assets/images/R.png"))),
+              Text(
                 "ITI Quiz App",
                 style: TextStyle(
                   color: Color.fromARGB(255, 126, 123, 160),
